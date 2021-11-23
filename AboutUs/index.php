3dc9b4f8cabf5f0f540b919c850acc7afbf4c2c1
AnandaFizalMuttaqin <72348884+AnandaFizalMuttaqin@users.noreply.github.com>
2021-11-23 17:30:00 +0700

    Add files via upload

--- AboutUs/index.php
+++ AboutUs/index.php
@@ -1,19 +1,11 @@
 <?php require "../../app/core/MVC_model.php";?>
 </head>
 <body>
-<?php require "$absolute_path/app/view/sidebar.php"; ?>
+<?php require "$absolute_path/app/views/sidebar.php"; ?>
+    <div class="body-container" style="height: auto;">
+        <center><h1><?= $data['namaDeskripsi'] ?></h1></center>
+        <center><img src="<?= BASE_URL ?>/Public/images/LOGO.png" alt=""></center>
         
-
-        <form action="" method="POST" enctype="multipart/form-data">
-            <div class="body-container container" data-aos="zoom-in">
-            <?= $message ?>
-            <center><input type="text" class="form-control" style="width: 300px;" name="judul" value="<?= $data['namaDeskripsi'] ?>" id=""></center>
-            <div class="mb-3">
-                <label for="">Edit Deskripsi</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" name="deskripsi" value="Deskripsi" rows="10"><?= $data['isiDeskripsi'] ?></textarea>
-            </div>
-            <center><button class="btn btn-success" value="simpan" type="submit" name="update">Simpan</button></center>
-            </div>
-        </form>
-<?php require "$absolute_path/app/view/footer.php"; ?>
-
+        <p><?= $data['isiDeskripsi'] ?></p>
+    </div>
+<?php require "$absolute_path/app/views/template/footer.php"; ?>
---
+++ DetailProduct/index.php
@@ -0,0 +1,9 @@
+<?php require "../../app/core/MVC_model.php";?>
+</head>
+<body>
+<?php require "$absolute_path/app/views/sidebar.php"; ?>
+    <div class="body-container" style="height: auto;">
+        <center>Detail Product</center>
+    </div>
+<?php require "$absolute_path/app/views/template/footer.php"; ?>
+
