92b609f85d28f049285c6e44b69feb6902576de0
AnandaFizalMuttaqin <72348884+AnandaFizalMuttaqin@users.noreply.github.com>
2021-11-23 17:19:55 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-$absolute_path = realpath($_SERVER["DOCUMENT_ROOT"])."/sidemand";
-// echo  $absolute_path;
-require "$absolute_path/app/core/MVC_model.php";
-require "$absolute_path/app/controller/Super_Default_method.php";
+$absolute_path = realpath($_SERVER["DOCUMENT_ROOT"])."/sidemand/Admin";
+require "$absolute_path/app/controller/pass_url.php";
+?>
